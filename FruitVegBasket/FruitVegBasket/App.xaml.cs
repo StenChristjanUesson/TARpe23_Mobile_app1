ee45f95664dc627b7c420438477e1f1a6962cb5c
Opilane TTHK <34353363+StenChristjanUesson@users.noreply.github.com>
2025-02-25 11:15:17 +0200

    FruitVegBasket was added

---
+++ FruitVegBasket/FruitVegBasket/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FruitVegBasket
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
