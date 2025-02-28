7b7691f45ae716794915b13a8367065e631adfba
Opilane TTHK <34353363+StenChristjanUesson@users.noreply.github.com>
2025-02-28 14:12:13 +0200

    Part 1 Grocery App
    e-Commerce Grocery App using .Net MAUI | Project Setup, Tabs & Models

---
+++ FruitVegBasket/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
+namespace FruitVegBasket
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            return builder.Build();
+        }
+    }
+}
+//This is the Video git code:
+//https://github.com/Abhayprince/FruitVegBasketMAUI
