9dd3132485ecae77555291cf89b3588755274e3a
Opilane TTHK <34353363+StenChristjanUesson@users.noreply.github.com>
2025-03-03 13:35:00 +0200

    Part 1 is finished

---
+++ FruitVegBasket/Models/CartItem.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FruitVegBasket.Models
+{
+    public partial class CartItem : ObservableObject
+    {
+        public Guid Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+
+        //public int Quantity { get; set; }
+
+        [ObservableProperty, NotifyPropertyChangedFor(nameof(Amount))]
+        public int _quantity;
+        public decimal Amount => Price * Quantity;
+    }
+}
